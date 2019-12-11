579a86466cb51a951972ffea0ac31f9bab7cdd0d
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2019-12-11 22:31:09 +0900

    Update LC 222 a faster method

--- src/com/package1/LC222.java
+++ src/com/package1/LC222.java
@@ -14,22 +14,42 @@ public class LC222 {
     }
 
     private int size = 0;
-
+    private int depth = 0;
     public int countNodes(TreeNode root) {
 
+        getDepth(root,0);
         countNodes(root,0);
 
         return size;
     }
 
     private void countNodes(TreeNode root, int d){
-        if (root == null)
+        if (root == null) return;
+        if (d == depth-1){
+            size++;
+            int count = getCountOfChild(root);
+            size += count;
             return;
+        }
         size++;
         countNodes(root.left,d+1);
         countNodes(root.right,d+1);
     }
 
+    private void getDepth(TreeNode root, int d){
+        if (root == null){
+            depth = d-1;
+            return;
+        }
+        getDepth(root.left, d+1);
+    }
+
+    private int getCountOfChild(TreeNode root){
+        int count = 0;
+        if (root.left != null) count++;
+        if (root.right != null) count++;
+        return count;
+    }
 
 }
 

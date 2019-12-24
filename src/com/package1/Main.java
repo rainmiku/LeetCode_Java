2198b716dd47fa6515c4cf104951429a7ca65937
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2019-12-24 17:57:23 +0900

    LC122

--- src/com/package1/Main.java
+++ src/com/package1/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var solution = new LC121();
+        var solution = new LC122();
 
-        int[] prices = {7,6,4,3,1};
+        int[] prices = {7,6,5,4};
 
 
         System.out.println(solution.maxProfit(prices));

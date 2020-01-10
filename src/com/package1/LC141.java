845bbd5f54152b56d1f319314524f32110e2ed7b
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-01-10 13:52:35 +0900

    LC313

---
+++ src/com/package1/LC141.java
@@ -0,0 +1,18 @@
+package com.package1;
+
+public class LC141 {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast!=null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+}

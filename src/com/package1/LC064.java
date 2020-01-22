d38a3ea2394365b830c852b1324b95b5da63d5a1
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-01-22 17:06:11 +0900

    LC064

--- src/com/package1/LC064.java
+++ src/com/package1/LC064.java
@@ -1,24 +1,24 @@
 package com.package1;
 
 public class LC064 {
-    private int max = 0;
     public int minPathSum(int[][] grid) {
-        minPathSum(grid,0,0,grid[0][0]);
-        return max;
-    }
 
-    private void minPathSum(int[][] grid, int i, int j,int sum){
-        if (i==grid.length-1 && j==grid[0].length-1)
-        {
-            if (sum > max)
-                max = sum;
-            return;
+       int[][] f = new int[grid.length][grid[0].length];
+
+        f[0][0] = -grid[0][0];
+        for (int i = 1; i < grid.length; i++) {
+            f[i][0] = f[i-1][0]-grid[i][0];
+        }
+        for (int i = 1; i < grid[0].length; i++) {
+            f[0][i] = f[0][i-1]-grid[0][i];
         }
 
-        if (i == grid.length || j == grid[0].length)
-            return;
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                f[i][j] = Math.max(f[i-1][j],f[i][j-1])-grid[i][j];
+            }
+        }
 
-        minPathSum(grid,i+1,j,sum+grid[i+1][j]);
-        minPathSum(grid,i,j+1,sum+grid[i][j+1]);
+        return -f[grid.length-1][grid[0].length-1];
     }
 }

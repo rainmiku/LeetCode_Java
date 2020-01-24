9740adb5a551dd6711b97d5e875b88ecefbd51db
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-01-24 15:11:40 +0900

    LC226

---
+++ src/com/package1/LC226.java
@@ -0,0 +1,12 @@
+package com.package1;
+
+public class LC226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+
+}

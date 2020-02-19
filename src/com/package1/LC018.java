98faf6c4029dfd9f0d5326d7d4b70e5f28e98148
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-02-19 15:58:01 +0900

    LC018

---
+++ src/com/package1/LC018.java
@@ -0,0 +1,35 @@
+package com.package1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC018 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> quadruplets = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length-3; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i+1; j < nums.length-2; j++) {
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                int left = j+1, right = nums.length-1;
+                while (left < right){
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target){
+                        quadruplets.add(new ArrayList<>(Arrays.asList(nums[i],nums[j],nums[left++],nums[right--])));
+                        while (left < right && nums[left] == nums[left-1])
+                            left++;
+                        while (left < right && nums[right] == nums[right+1])
+                            right--;
+                    } else if (sum < target)
+                        left++;
+                    else
+                        right--;
+                }
+            }
+        }
+        return quadruplets;
+    }
+}

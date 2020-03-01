a531da4b38c24f86fec0394d4c5d6ef934e99638
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-03-01 20:12:43 +0900

    LC112

---
+++ src/com/package1/LC112.java
@@ -0,0 +1,10 @@
+package com.package1;
+
+public class LC112 {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        if (sum == root.val && root.left== null && root.right == null ) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}

2592a9ca76953572487674716c53e0ca4f229477
Rainatsu <yu.xia@ar.c.titech.ac.jp>
2020-03-13 17:20:51 +0900

    LC744

---
+++ src/com/package1/LC744.java
@@ -0,0 +1,24 @@
+package com.package1;
+
+public class LC744 {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0, end = letters.length-1;
+
+        while (start <= end){
+            int mid = start + (end-start)/2;
+            if (letters[mid] == target){
+                start = mid+1;
+                break;
+            }
+            if (letters[mid] < target)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        while (start>0 && start < letters.length && letters[start] == letters[start-1])
+            start++;
+
+        return letters[start % letters.length];
+
+    }
+}

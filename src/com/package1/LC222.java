e1015811f4b31ccb2b9c9ac30138afa3db09363e
rainatsu <rain19941220@gmail.com>
2021-06-03 19:50:50 +0900

    Update LC222

--- src/com/package1/LC222.java
+++ src/com/package1/LC222.java
@@ -5,50 +5,26 @@ import java.util.Queue;
 
 public class LC222 {
 
-
-    public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
-    }
-
-    private int size = 0;
-    private int depth = 0;
     public int countNodes(TreeNode root) {
+        int leftDepth = 0;
+        int rightDepth = 0;
 
-        getDepth(root,0);
-        countNodes(root,0);
-
-        return size;
-    }
-
-    private void countNodes(TreeNode root, int d){
-        if (root == null) return;
-        if (d == depth-1){
-            size++;
-            int count = getCountOfChild(root);
-            size += count;
-            return;
+        TreeNode current = root;
+        while (current != null){
+            leftDepth++;
+            current = current.left;
         }
-        size++;
-        countNodes(root.left,d+1);
-        countNodes(root.right,d+1);
-    }
 
-    private void getDepth(TreeNode root, int d){
-        if (root == null){
-            depth = d-1;
-            return;
+        current = root;
+        while (current != null){
+            rightDepth++;
+            current = current.right;
         }
-        getDepth(root.left, d+1);
-    }
 
-    private int getCountOfChild(TreeNode root){
-        int count = 0;
-        if (root.left != null) count++;
-        if (root.right != null) count++;
-        return count;
+        if(leftDepth == rightDepth)
+            return (int)Math.pow(2,leftDepth)-1;
+
+        return 1+countNodes(root.left)+countNodes(root.right);
     }
 
 }

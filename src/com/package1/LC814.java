4b5a85ac8f74c9f7ba3a9e1bae2c0d886306d23d
Rainatsu <rain19941220@gmail.com>
2021-06-04 15:05:43 +0900

    LC814

---
+++ src/com/package1/LC814.java
@@ -0,0 +1,15 @@
+package com.package1;
+
+public class LC814 {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.val == 1 || root.left != null || root.right != null ) return root;
+
+        return null;
+    }
+
+}

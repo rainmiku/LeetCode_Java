6a12c8fbc681e49bb1e8fb5eed94ad033077ba38
rainatsu <rain19941220@gmail.com>
2021-06-29 23:19:27 +0900

    LC168/LC171

---
+++ src/com/package1/LC168.java
@@ -0,0 +1,13 @@
+package com.package1;
+
+public class LC168 {
+    public String convertToTitle(int columnNumber) {
+        String ans = "";
+        while (columnNumber > 0){
+            columnNumber--;
+            ans = (char)(columnNumber % 26 + 'A') + ans;
+            columnNumber = columnNumber / 26;
+        }
+        return ans;
+    }
+}
